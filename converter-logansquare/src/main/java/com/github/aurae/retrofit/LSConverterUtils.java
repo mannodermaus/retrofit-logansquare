21f7500fed58dea1a2d36eeac6f8bc20f65d5b4f
Marcel Schnelle <ms@planetmutlu.com>
2015-10-15 14:18:31 +0200

    Initial commit

---
+++ converter-logansquare/src/main/java/com/github/aurae/retrofit/LSConverterUtils.java
@@ -0,0 +1,7 @@
+package com.github.aurae.retrofit;
+
+import com.squareup.okhttp.MediaType;
+
+abstract class LSConverterUtils {
+	static final MediaType MEDIA_TYPE = MediaType.parse("application/json; charset=UTF-8");
+}
