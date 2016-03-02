130b6a4334b3d60e658fcd1d534c4fd83a896fd0
Jawnnypoo <jawnnypoo@gmail.com>
2016-03-02 08:42:12 -0600

    Actually check instanceof properly

--- converter-logansquare/src/main/java/com/github/aurae/retrofit2/LoganSquareConverterFactory.java
+++ converter-logansquare/src/main/java/com/github/aurae/retrofit2/LoganSquareConverterFactory.java
@@ -28,7 +28,10 @@ public final class LoganSquareConverterFactory extends Converter.Factory {
 
     @Override
     public Converter<ResponseBody, ?> responseBodyConverter(Type type, Annotation[] annotations, Retrofit retrofit) {
-        if (!LoganSquare.supports((Class) type)) {
+        if (type instanceof Class && !LoganSquare.supports((Class) type)) {
+            return null;
+        }
+        if (type instanceof ParameterizedType && !LoganSquare.supports((ParameterizedType) type)) {
             return null;
         }
         return new LoganSquareResponseBodyConverter(type);
@@ -36,7 +39,10 @@ public final class LoganSquareConverterFactory extends Converter.Factory {
 
     @Override
     public Converter<?, RequestBody> requestBodyConverter(Type type, Annotation[] parameterAnnotations, Annotation[] methodAnnotations, Retrofit retrofit) {
-        if (!LoganSquare.supports((Class) type)) {
+        if (type instanceof Class && !LoganSquare.supports((Class) type)) {
+            return null;
+        }
+        if (type instanceof ParameterizedType && !LoganSquare.supports((ParameterizedType) type)) {
             return null;
         }
         return new LoganSquareRequestBodyConverter(type);
