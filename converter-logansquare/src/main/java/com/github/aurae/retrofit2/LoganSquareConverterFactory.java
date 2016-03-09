4ef7c861a5ac0eafb1eece406abf2ee18e842c16
Marcel Schnelle <ms@planetmutlu.com>
2016-03-09 12:42:18 +0100

    Refined checking for supported types of the converter, removing the burden from the converters and placing it in the Factory that distributes the converters; also added unit tests for "not supported" cases

--- converter-logansquare/src/main/java/com/github/aurae/retrofit2/LoganSquareConverterFactory.java
+++ converter-logansquare/src/main/java/com/github/aurae/retrofit2/LoganSquareConverterFactory.java
@@ -1,5 +1,7 @@
 package com.github.aurae.retrofit2;
 
+import com.bluelinelabs.logansquare.LoganSquare;
+import com.bluelinelabs.logansquare.ParameterizedType;
 import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Converter;
@@ -7,6 +9,8 @@ import retrofit2.Retrofit;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -17,6 +21,7 @@ import java.lang.reflect.Type;
 public final class LoganSquareConverterFactory extends Converter.Factory {
     /**
      * Create an instance. Encoding to JSON and decoding from JSON will use UTF-8.
+     *
      * @return A {@linkplain Converter.Factory} configured to serve LoganSquare converters
      */
     public static LoganSquareConverterFactory create() {
@@ -26,25 +31,60 @@ public final class LoganSquareConverterFactory extends Converter.Factory {
     private LoganSquareConverterFactory() {
     }
 
+    private boolean isSupported(Type type) {
+        // Check ordinary Class
+        if (type instanceof Class && !LoganSquare.supports((Class) type)) {
+            return false;
+        }
+
+        // Check LoganSquare's ParameterizedType
+        if (type instanceof ParameterizedType && !LoganSquare.supports((ParameterizedType) type)) {
+            return false;
+        }
+
+        // Check target types of java.lang.reflect.ParameterizedType
+        if (type instanceof java.lang.reflect.ParameterizedType) {
+            java.lang.reflect.ParameterizedType pt = (java.lang.reflect.ParameterizedType) type;
+            Type[] typeArguments = pt.getActualTypeArguments();
+            Type firstType = typeArguments[0];
+
+            Type rawType = pt.getRawType();
+            if (rawType == Map.class) {
+                // LoganSquare only handles Map objects with String keys and supported types
+                Type secondType = typeArguments[1];
+                if (firstType != String.class || !isSupported(secondType)) {
+                    return false;
+                }
+
+            } else if (rawType == List.class) {
+                // LoganSquare only handles List objects of supported types
+                if (!isSupported(firstType)) {
+                    return false;
+                }
+
+            } else {
+                // TODO Generics
+            }
+        }
+
+        return true;
+    }
+
     @Override
     public Converter<ResponseBody, ?> responseBodyConverter(Type type, Annotation[] annotations, Retrofit retrofit) {
-        if (type instanceof Class && !LoganSquare.supports((Class) type)) {
+        if (isSupported(type)) {
+            return new LoganSquareResponseBodyConverter(type);
+        } else {
             return null;
         }
-        if (type instanceof ParameterizedType && !LoganSquare.supports((ParameterizedType) type)) {
-            return null;
-        }
-        return new LoganSquareResponseBodyConverter(type);
     }
 
     @Override
     public Converter<?, RequestBody> requestBodyConverter(Type type, Annotation[] parameterAnnotations, Annotation[] methodAnnotations, Retrofit retrofit) {
-        if (type instanceof Class && !LoganSquare.supports((Class) type)) {
+        if (isSupported(type)) {
+            return new LoganSquareRequestBodyConverter(type);
+        } else {
             return null;
         }
-        if (type instanceof ParameterizedType && !LoganSquare.supports((ParameterizedType) type)) {
-            return null;
-        }
-        return new LoganSquareRequestBodyConverter(type);
     }
 }
--- converter-logansquare/src/main/java/com/github/aurae/retrofit2/LoganSquareRequestBodyConverter.java
+++ converter-logansquare/src/main/java/com/github/aurae/retrofit2/LoganSquareRequestBodyConverter.java
@@ -6,10 +6,7 @@ import okhttp3.RequestBody;
 import retrofit2.Converter;
 
 import java.io.IOException;
-import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.List;
-import java.util.Map;
 
 final class LoganSquareRequestBodyConverter implements Converter<Object, RequestBody> {
 
@@ -23,15 +20,6 @@ final class LoganSquareRequestBodyConverter implements Converter<Object, Request
 
     @Override
     public RequestBody convert(Object value) throws IOException {
-        if (type instanceof ParameterizedType) {
-            // Check for generics types
-            ParameterizedType parameterizedType = (ParameterizedType) type;
-            Type rawType = parameterizedType.getRawType();
-            if (rawType != List.class && rawType != Map.class) {
-                // TODO Generics
-            }
-        }
-
         // For general cases, use the central LoganSquare serialization method
         return RequestBody.create(MEDIA_TYPE, LoganSquare.serialize(value));
     }
